2fdfee7b103ad8a0fa139d3d4a5eea25f1e9b7ff
Donald Perry <dperry@kewlmagic.com>
2011-09-03 12:50:07 +0000

    awesome caught a bug

--- controller.rb
+++ controller.rb
@@ -36,7 +36,7 @@ class Controller < Autumn::Leaf
   
   def listprojects_command(stem, sender, reply_to, msg)
     
-    var projects = Projects.all
+    var :projects => Project.all
     
   end
   
